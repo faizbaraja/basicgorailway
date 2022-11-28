7dc4421b92e83aab5bb31dfb9c5b97bdde949fb3
faiz baraja <faiz.baraja89@gmail.com>
2022-11-28 09:56:35 +0700

    use handle func

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World! faiz baraja")
-	})
-
 	envErr := godotenv.Load(".env")
 	if envErr != nil {
 		fmt.Printf("error load enf file")
@@ -22,6 +22,24 @@ func main() {
 
 	port := os.Getenv("PORT")
 	appPort := "0.0.0.0:" + port
-	fmt.Println(appPort)
-	http.ListenAndServe(appPort, nil)
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(appPort, nil))
 }
+
+// func main() {
+// 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 	// 	fmt.Fprintf(w, "Hello World! faiz baraja")
+// 	// })
+
+// 	// envErr := godotenv.Load(".env")
+// 	// if envErr != nil {
+// 	// 	fmt.Printf("error load enf file")
+// 	// 	os.Exit(1)
+// 	// }
+
+// 	// port := os.Getenv("PORT")
+// 	// appPort := "0.0.0.0:" + port
+// 	// fmt.Println(appPort)
+// 	// http.ListenAndServe(appPort, nil)
+// }
